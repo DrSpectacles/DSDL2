c9c53b7663196cde1942d6c4f8050ad47e016350
DrSpectacles <misiaki@dbcon.eu>
2021-10-14 14:37:38 +0100

    completed dot adder

--- dot_adder.v
+++ dot_adder.v
@@ -1,4 +1,24 @@
-module dot_adder(input [6:0] pattern,
+module dot_adder(
+		 input [6:0]  pattern,
+		 input [3:0]  dots,
+		 input [2:0]  dig_sel,
+		 output [7:0] segments
 		 
 		 );
    
+   always @ (pattern, dots, dig_sel)
+     case (dig_sel)
+       
+       2'b00: output = { pattern[6:0], dots[0] };
+
+       2'b01: output = { pattern[6:0], dots[1] };
+       
+       2'b10: output = { pattern[6:0], dots[2] };
+       
+       2'b11: output = { pattern[6:0], dots[3] };
+       
+       
+
+     endcase (dig_sel); // case (dig_sel)
+
+endmodule
