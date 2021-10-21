38cb24fa922a2a713001a8e5bb5f63b502ac7773
DrSpectacles <78048895+DrSpectacles@users.noreply.github.com>
2021-10-21 18:56:38 +0100

    Update count12.v

--- count12.v
+++ count12.v
@@ -1,30 +1,32 @@
-// welcome to the jungle
-//Block 1
+// Block 1
+// Module to generate signal that will select a digit
+// and switch digits at ~1.2kHz
+
 module count12(input clock,
 	       input  reset,
-	       output reg [1:0] dig_sel
+	       output reg [1:0] dig_sel // 2 bit output
 	       );
    
-   reg [11:0]		  counter;
-   wire			  pulse;
-   
-
+	reg [11:0]		  counter;
+	// a 12 bit counter was chosen to get an appropriate
+	// display refresh rate
+        wire			  pulse; 
    
    always @ (posedge clock)
      begin
-	if (reset) counter <= 12'd0;
-	else
-
-	  counter <= counter + 12'd1;
+	if (reset) counter <= 12'd0; //synchronous reset	
+	else	
+	  counter <= counter + 12'd1; //advance count
      end
-   
-   assign pulse = (counter == 12'd4095);
+	assign pulse = (counter == 12'd4095); 
+	// pulse is sent out at end of 12 bit count
 
-   always @ (posedge clock)
+	
+	always @ (posedge clock) //2 bit counter   
      begin
 	if (reset) dig_sel <= 2'd0;
 	else
-	  if (pulse) dig_sel <= dig_sel + 2'd1;
+	if (pulse) dig_sel <= dig_sel + 2'd1; //only advance when pulse high
 	  else dig_sel <= dig_sel;
      end
 	
