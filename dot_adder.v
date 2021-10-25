837e27e010ec0ef278f1033723800ac3f2d89327
DrSpectacles <78048895+DrSpectacles@users.noreply.github.com>
2021-10-25 19:14:36 +0100

    Update dot_adder.v

--- dot_adder.v
+++ dot_adder.v
@@ -1,16 +1,18 @@
-//Block 5 
 
-module dot_adder(
+//Block 5 
+// Module to add the dot to the display signal
+
+module dot_adder(	
 		 input [6:0]  pattern,
 		 input [3:0]  dots,
-  input [1:0]  dig_sel,
-  	     output reg [7:0] segments 
-		 );
+  		 input [1:0]  dig_sel,
+  	     	 output reg [7:0] segments 
+		 );		
    
   always @ (dig_sel, pattern, dots) 
      case (dig_sel)
        
-       2'b00: segments = { pattern, !(dots[0]) };
+	     2'b00: segments = { pattern, !(dots[0]) };  // dot sigal needs to be inverted before adding
 
        2'b01: segments = { pattern, !(dots[1]) };
        
