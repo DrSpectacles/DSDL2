44af35eff4ccb4ecc6def4d8cadd60cb994777a4
DrSpectacles <78048895+DrSpectacles@users.noreply.github.com>
2021-10-25 19:11:33 +0100

    Update digit_signal_generator.v

--- digit_signal_generator.v
+++ digit_signal_generator.v
@@ -1,11 +1,13 @@
-// welcome to the jungle
+
 //Block 2
+// Signal to enable or disable each digit of the display
+
 module digit_signal_generator(input [1:0] dig_sel,
                               output reg [7:0] digit
 			      );
 
    always @ (dig_sel)
-     case (dig_sel)
+	   case (dig_sel) // Designed for an 8 digit display with only 4 used
        2'b00: digit = 8'b11111110;
        2'b01: digit = 8'b11111101;
        2'b10: digit = 8'b11111011;
